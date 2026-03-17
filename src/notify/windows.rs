fc39353593eaa29b68b1b5c3c76e2484f905c0dc
Robert Roskam <raiderrobert@gmail.com>
2026-03-16 21:16:59 -0400

    refactor: remove Linux/Windows notification stubs (macOS-only for now) (#16)

--- src/notify/windows.rs
+++
@@ -1,7 +0,0 @@
-use anyhow::Result;
-
-use crate::client::message::Notification;
-
-pub fn show(_notification: &Notification) -> Result<()> {
-    anyhow::bail!("Windows notifications not yet implemented")
-}
