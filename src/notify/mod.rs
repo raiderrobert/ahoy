fd0e89de03514f255d778a1101904776f1101650
rroskam <raiderrobert@gmail.com>
2026-03-17 09:35:43 -0400

    chore: remove Rust codebase, old Swift source, and stale config

--- src/notify/mod.rs
+++
@@ -1,9 +0,0 @@
-mod macos;
-
-use crate::client::message::Notification;
-use anyhow::Result;
-
-/// Show a native macOS notification
-pub fn show(notification: &Notification) -> Result<()> {
-    macos::show(notification)
-}
